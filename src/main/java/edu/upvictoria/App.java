6671fb537cc9f5a0b7811cb7dc1cfcfcbd1232b6
soriane <ricardo.roel110@gmail.com>
2024-06-03 11:36:36 -0600

    first commit

---
+++ src/main/java/edu/upvictoria/App.java
@@ -0,0 +1,13 @@
+package edu.upvictoria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
