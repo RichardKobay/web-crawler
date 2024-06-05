44a47bbe1972b8cd6ad7a63d69111e3ef6a7f168
richardkobay <ricardo.roel110@gmail.com>
2024-06-05 11:41:59 -0600

    ivan

--- src/main/java/edu/upvictoria/App.java
+++ src/main/java/edu/upvictoria/App.java
@@ -1,5 +1,12 @@
 package edu.upvictoria;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+
+            Document doc = Jsoup.connect("https://www.imdb.com/title/tt1392190/?ref_=hm_tpks_tt_i_1_pd_tp1_pbr_ic").get();
+            // Gets the title
+            System.out.println(doc.title());
+
+            // Gets all the links
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                System.out.println(link.attr("abs:href"));
+            }
+
+
+            // Element tittle = doc.select("span.hero__primary-text[data-testid=hero__primary-text]").first();
+            // Elements actorLinks = doc.select("a.ipc-metadata-list-item__list-content-item.ipc-metadata-list-item__list-content-item--link");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
